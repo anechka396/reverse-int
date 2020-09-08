c471248b8a749037bda7ba1a0e7b7ff4fa7cee95
Anna Yakubenko <ayakubenko@atypicaldigital.com>
2020-09-08 23:59:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+	if(n < 0) n *= -1;
+	var reverse_n = n%10; 
+	while(Math.trunc(n/10) != 0) {
+		n = Math.trunc(n/10);
+		reverse_n = reverse_n*10 + n%10;
+	} 
+	return reverse_n;
 }
